5cc48abdf11f96de1644120df6080abdf27f4e83
PrathameshSam <sam.pratham3@gmail.com>
2021-06-16 13:29:47 +0530

    Adds simple mvn file

--- vars/simpleMvnBuild.groovy
+++ vars/simpleMvnBuild.groovy
@@ -1,6 +1,6 @@
 def call(Map config){
     node {
-                sh "mvn clean install -P${config.ENV} -DskipTest"
+            sh "mvn clean install -P${config.ENV} -DskipTest"
         }
     }
 }
