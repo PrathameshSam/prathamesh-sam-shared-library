2c38e16f3814634c0a816b2cb052ff2df99a1b44
PrathameshSam <sam.pratham3@gmail.com>
2021-06-28 15:49:57 +0530

    Build

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,8 +1,8 @@
-def call(String env, String gcp){
+def call(Map [] config){
     node{
         sh '''
         #mvn -B -DskipTests clean package
-        echo "Building the project for ${env} ,${gcp}"
+        echo "Building the project for ${config.crawler_env} ,${config.gcp_project}"
         '''
     }
 } 
