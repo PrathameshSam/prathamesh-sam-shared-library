8060a7e9b0ccedba6a677e3866b855a2acfecb10
PrathameshSam <sam.pratham3@gmail.com>
2021-06-28 15:42:51 +0530

    Build

--- mavenBuild.groovy
+++ mavenBuild.groovy
@@ -1,7 +1,10 @@
 def call(String [] args){
     stages{
         stage('Build'){
-            sh 'mvn -B -DskipTests clean package'
+            sh '''
+            mvn -B -DskipTests clean package
+            echo "Building the project for ${agrs[0]} ,${args[1]}"
+            '''
         }
     }
 }
